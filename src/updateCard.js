ae578d4048ac371e03f64cf0b792f516bc2a16bf
nishantjo-c <nishantjoshiofficial@gmail.com>
2023-04-17 23:32:00 +0530

    title author cover fetched

--- src/updateCard.js
+++ src/updateCard.js
@@ -1,11 +1,12 @@
 import updateCardCSS from './styles/updateCard.module.css'
 
 function UpdateCard(props){
-	console.log(props.authorName)
+	const coverVal = props.coverVal;
 	return (
 		<div className={updateCardCSS.container}>
 			<h2>{props.bookTitle}</h2>
 			<p>{props.authorName}</p>
+			<img src={coverVal} alt="no image yet!" />
 		</div>
 	);}
 
--- src/updates.js
+++ src/updates.js
@@ -5,27 +5,38 @@ import UpdateCard from './updateCard.js'
 
 function Updates(){
 
+//	defined states
+
 	const [data, setData] = useState(''); 
 	const [bookTitle, setBookTitle] = useState('no title!');
+	const [coverVal, setCoverVal] = useState(null);
 	const [authorName, setAuthorName] = useState('no author name!');
 
 	useEffect(() => {
 		const data = async () => {
-		const response = await fetch('https://openlibrary.org/search.json?title=a+little+life')
+		const response = await fetch('https://openlibrary.org/search.json?title=if+tomorrow+comes')
 		const data = await response.json();
 		setData(data);
 		return data;
 		}
 		data().then(data => {
 			data = data.docs;
+/*
+//	Adding '+' to wide spaces
+			let str = data[0].title;
+			let newStr = str.replace(/\s+/g, "+");
+			console.log(newStr)
+			console.log(data[0])
+*/
 			setBookTitle(data[0].title);
 			setAuthorName(data[0].author_name[0]);
+			setCoverVal(`https://covers.openlibrary.org/b/isbn/${data[0].isbn[0]}-M.jpg`);
 		});
 	},[])
 	
 	return (
 		<div className={updatesCSS.container}>
-			<UpdateCard authorName={authorName} bookTitle={bookTitle}/>
+			<UpdateCard authorName={authorName} bookTitle={bookTitle} coverVal={coverVal}/>
 		</div>
 	);
 }
