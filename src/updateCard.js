a8e14466c5a5f8750e7ede7388d63bfe655f6a13
nishantjo-c <nishantjoshiofficial@gmail.com>
2023-04-17 18:59:48 +0530

    data accessed

--- src/updateCard.js
+++ src/updateCard.js
@@ -1,9 +1,11 @@
 import updateCardCSS from './styles/updateCard.module.css'
 
-function UpdateCard(){
+function UpdateCard(props){
+	console.log(props.authorName)
 	return (
 		<div className={updateCardCSS.container}>
-			<p>yo...</p>
+			<h2>{props.bookTitle}</h2>
+			<p>{props.authorName}</p>
 		</div>
 	);}
 
--- src/updates.js
+++ src/updates.js
@@ -4,21 +4,28 @@ import updatesCSS from './updates.module.css';
 import UpdateCard from './updateCard.js'
 
 function Updates(){
+
 	const [data, setData] = useState(''); 
+	const [bookTitle, setBookTitle] = useState('no title!');
+	const [authorName, setAuthorName] = useState('no author name!');
 
 	useEffect(() => {
-		fetch('https://openlibrary.org/search.json?title=a+little+life')
-		.then(res => res.json())
-		.then(data => {
-			setData(data);
-		})
+		const data = async () => {
+		const response = await fetch('https://openlibrary.org/search.json?title=a+little+life')
+		const data = await response.json();
+		setData(data);
+		return data;
+		}
+		data().then(data => {
+			data = data.docs;
+			setBookTitle(data[0].title);
+			setAuthorName(data[0].author_name[0]);
+		});
 	},[])
-
-	console.log(data.docs);
-
+	
 	return (
 		<div className={updatesCSS.container}>
-			<UpdateCard />
+			<UpdateCard authorName={authorName} bookTitle={bookTitle}/>
 		</div>
 	);
 }
